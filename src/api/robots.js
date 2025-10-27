34cd5b359fc6cc97d5a2b7d7e10999e7b6212506
eri2113-source <eri.2113@gmail.com>
2025-10-27 17:53:36 -0300

    🔧 Corrigir 404 sitemap - função serverless moderna

--- src/api/robots.js
+++ src/api/robots.js
@@ -1,5 +1,5 @@
-// Vercel Serverless Function - SINTAXE CORRETA!
-module.exports = (req, res) => {
+// Vercel Serverless Function - Robots.txt
+export default function handler(req, res) {
   const robotsTxt = `# VolleyPro - Robots.txt
 User-agent: *
 Allow: /
@@ -9,6 +9,6 @@ Sitemap: https://voleypro.net/sitemap.xml
 Crawl-delay: 1`;
 
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.setHeader('Cache-Control', 'public, max-age=3600');
-  res.status(200).end(robotsTxt);
-};
+  res.setHeader('Cache-Control', 'public, max-age=3600, s-maxage=3600');
+  res.status(200).send(robotsTxt);
+}
--- src/api/sitemap.js
+++ src/api/sitemap.js
@@ -1,52 +1,52 @@
-// Vercel Serverless Function - SINTAXE CORRETA!
-module.exports = (req, res) => {
+// Vercel Serverless Function - Sitemap XML
+export default function handler(req, res) {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://voleypro.net/</loc>
-    <lastmod>2025-10-26</lastmod>
+    <lastmod>2025-10-27</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>https://voleypro.net/#feed</loc>
-    <lastmod>2025-10-26</lastmod>
+    <lastmod>2025-10-27</lastmod>
     <changefreq>hourly</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
     <loc>https://voleypro.net/#showcase</loc>
-    <lastmod>2025-10-26</lastmod>
+    <lastmod>2025-10-27</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
     <loc>https://voleypro.net/#teams</loc>
-    <lastmod>2025-10-26</lastmod>
+    <lastmod>2025-10-27</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
     <loc>https://voleypro.net/#tournaments</loc>
-    <lastmod>2025-10-26</lastmod>
+    <lastmod>2025-10-27</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
     <loc>https://voleypro.net/#lives</loc>
-    <lastmod>2025-10-26</lastmod>
+    <lastmod>2025-10-27</lastmod>
     <changefreq>hourly</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
     <loc>https://voleypro.net/#monetization</loc>
-    <lastmod>2025-10-26</lastmod>
+    <lastmod>2025-10-27</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.7</priority>
   </url>
 </urlset>`;
 
   res.setHeader('Content-Type', 'application/xml; charset=utf-8');
-  res.setHeader('Cache-Control', 'public, max-age=3600');
-  res.status(200).end(sitemap);
-};
+  res.setHeader('Cache-Control', 'public, max-age=3600, s-maxage=3600');
+  res.status(200).send(sitemap);
+}
